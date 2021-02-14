662b8aeef6b3c81569a621ecdbc5aafeaceb995c
SUMIT KAWALE <sumitkawale2002@gmail.com>
2021-02-15 02:11:02 +0530

    changed hello.py and added functions.py

---
+++ functions.py
@@ -0,0 +1,3 @@
+def sq(x):
+    print(f"{x}^2 = {x**2}")
+    return x**2
--- hello.py
+++ hello.py
@@ -1,2 +1,33 @@
-name = ["sumit", "amit", "mahesh"]
-print(name);
+# from functions import *
+import functions
+
+""" name = set();
+name.add(1)
+name.add(2)
+name.add(3)
+name.add(4)
+name.add(5)
+name.add(6)
+name.remove(2)
+print(name)
+print(f"the sent has {len(name)} elements.") """
+
+""" for i in [0, 1, 2, 3, 4, 5]:
+    print(i) """
+""" for i in range(6):
+    print(i) """
+
+""" name = ["sumit", "amit", "mahesh", "rohan"]
+for i in name:
+    print(i) """
+    
+""" student = {
+    'id':1,
+    'name': "sumit",
+    1:2
+}
+print(student)
+student['age'] = 19;
+print(student['age']) """
+# print(sq(4))
+print(functions.sq(4))
